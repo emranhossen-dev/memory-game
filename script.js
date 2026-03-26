4348151c989faadc8b30cb71f2233176d76a7e65
emran40989 <emran40989@gmail.com>
2026-03-27 03:58:18 +0600

    time super reduced

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ const sounds = {
 // 3. Level Configurations
 const levels = {
     1: { pairs: 6, time: 30, grid: 'grid-l1' },
-    2: { pairs: 10, time: 60, grid: 'grid-l2' },
-    3: { pairs: 12, time: 100, grid: 'grid-l3' },
-    4: { pairs: 14, time: 30, grid: 'grid-l4' }
+    2: { pairs: 10, time: 40, grid: 'grid-l2' },
+    3: { pairs: 12, time: 50, grid: 'grid-l3' },
+    4: { pairs: 14, time: 25, grid: 'grid-l4' }
 };
 
 // 4. Game State Variables
